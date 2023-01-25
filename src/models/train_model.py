c738dec665190815fc80ddfa0267152ab39b7a98
Andreas Theilgaard <actheilgaard@gmail.com>
2023-01-25 13:45:03 +0100

    precommmit v4

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
 
-ged = [1,2,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4]
+ged = [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
 
 
 def find_best_device():
