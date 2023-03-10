cc67971ef6d349a7f3bc3737b0acd5df58e9734a
Andreas Theilgaard <actheilgaard@gmail.com>
2023-03-10 05:05:46 +0100

    get data

---
+++ initialize.py
@@ -0,0 +1,42 @@
+import gdown
+import zipfile
+import os
+import subprocess
+
+def get_data():
+    if os.path.isdir('data') == False:
+        print("Downloading data")
+        process = subprocess.Popen('chmod +x ./get_data.sh',stdout=subprocess.PIPE,
+             stderr=subprocess.PIPE, shell = True)
+        stdout, stderr = process.communicate()
+        subprocess.call(['./get_data.sh'])
+    else:
+         print("Data already exists")
+get_data()
+
+
+def upload_file():
+    from pydrive.auth import GoogleAuth
+    from pydrive.drive import GoogleDrive
+    gauth = GoogleAuth()         
+    drive = GoogleDrive(gauth) 
+    gfile = drive.CreateFile({'parents': [{'id': '1UmD1_yE5nz5jcZPGDyK2iI-I-7a-P7Mj'}],'title':'hello123.txt'})
+    gfile.SetContentString('Hello Hello!')
+    gfile.Upload() # Upload the file.
+
+####
+# drive.ListFile('1IGN2yqPeUmJpzifrOE8Y4VMjn4Qmrr3x')
+# file_list = drive.ListFile({'q': "1IGN2yqPeUmJpzifrOE8Y4VMjn4Qmrr3x in parents and trashed=false"}).GetList()
+# file_list = drive.ListFile({'q': "'1w-TyCSLDGQbantlvK93imJaNABazXwy1' in parents and trashed=false"}).GetList()
+# file_list[0]
+
+# drive.ListFile({'q': "'1w-TyCSLDGQbantlvK93imJaNABazXwy1'"}).GetList()
+
+#gauth.LocalWebserverAuth()
+
+if __name__ == "__main__":
+    get_data()
+    upload_file()
+
+
+
