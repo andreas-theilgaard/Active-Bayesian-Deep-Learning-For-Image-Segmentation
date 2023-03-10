224e8ad835072ce0ae4e22c160710f5db8d16e2b
Andreas Theilgaard <actheilgaard@gmail.com>
2023-03-10 05:26:34 +0100

    update 2

--- tests/test_CI_data.py
+++ tests/test_CI_data.py
@@ -1,4 +1,4 @@
-import os
-def test_CI_Secret():
-    CLIENT_ID = os.environ['CLIENT_ID']
-    assert len(CLIENT_ID) == 72
+# import os
+# def test_CI_Secret():
+#     CLIENT_ID = os.environ['CLIENT_ID']
+#     assert len(CLIENT_ID) == 72
