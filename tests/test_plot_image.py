f0bb6e1884e208ea067edb1745b30d6f8418175e
Andreas Theilgaard <actheilgaard@gmail.com>
2023-03-12 18:45:17 +0100

    new branch

--- tests/test_plot_image.py
+++ tests/test_plot_image.py
@@ -1,23 +1,23 @@
-from torch.utils.data import Dataset
-from torchvision import transforms
-from PIL import Image
-import os
-import numpy as np
-import torch
-import matplotlib.pyplot as plt
+# from torch.utils.data import Dataset
+# from torchvision import transforms
+# from PIL import Image
+# import os
+# import numpy as np
+# import torch
+# import matplotlib.pyplot as plt
 
-img = "data/raw/warwick/image/0.bmp"
-img = Image.open(img)  # .convert("L")
+# img = "data/raw/warwick/image/0.bmp"
+# img = Image.open(img)  # .convert("L")
 
-img = np.array(img) / 255
+# img = np.array(img) / 255
 
-img.shape
+# img.shape
 
-plt.imshow(img)
-plt.show()
+# plt.imshow(img)
+# plt.show()
 
-plt.imshow(img, cmap="gray")
-plt.show()
+# plt.imshow(img, cmap="gray")
+# plt.show()
 
 
 # all
